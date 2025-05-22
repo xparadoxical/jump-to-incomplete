c44e74f58e5dcb116c63522d3e0c9da6be18965e
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2025-05-22 05:14:45 +0200

    Add shards support

---
+++ src/ShardsPage.cpp
@@ -0,0 +1,70 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/ShardsPage.hpp>
+#include "JumpButton.h"
+
+using namespace geode::prelude;
+
+enum class ShardType
+{
+    Fire = 18,
+    Ice = 19,
+    Poison = 17,
+    Shadow = 16,
+    Lava = 20,
+    Earth = 23,
+    Blood = 24,
+    Metal = 25,
+    Light = 26,
+    Soul = 27
+};
+
+std::array g_shardTypes = {ShardType::Fire, ShardType::Ice, ShardType::Poison, ShardType::Shadow, ShardType::Lava, ShardType::Earth, ShardType::Blood, ShardType::Metal, ShardType::Light, ShardType::Soul};
+
+class $modify(MyShardsPage, ShardsPage)
+{
+    struct Fields
+    {
+        std::array<int, g_shardTypes.size()> shards;
+    };
+
+    $override bool init()
+    {
+        if (!ShardsPage::init())
+            return false;
+        
+        auto gsm = GameStatsManager::sharedState();
+        for (int i = 0; i < g_shardTypes.size(); i++)
+            m_fields->shards[i] = gsm->getStat(fmt::to_string((int)g_shardTypes[i]).c_str());
+
+        for (int page = m_page + 1; page < m_pages->count(); page++)
+        {
+            switch (page)
+            {
+                case 1:
+                    if (*std::ranges::min_element(m_fields->shards.begin(), m_fields->shards.begin() + 5) < 100)
+                        goto jump;
+                    break;
+                case 2:
+                    if (std::min({m_fields->shards[5], m_fields->shards[6], m_fields->shards[7]}) < 100)
+                        goto jump;
+                    break;
+                case 3:
+                    if (*std::ranges::min_element(m_fields->shards.begin() + 5, m_fields->shards.begin() + 10) < 100)
+                        goto jump;
+                    break;
+                default:
+                    log::warn("unhandled ShardsPage page {}", page);
+            }
+
+            continue;
+
+        jump:
+            auto onSwitchPage = reinterpret_cast<void(*)(ShardsPage*,CCObject*)>(geode::base::get() + 0x474650);
+            for (int i = m_page; i < page; i++)
+                onSwitchPage(this, m_nextButton);
+            break;
+        }
+
+        return true;
+    }
+};
