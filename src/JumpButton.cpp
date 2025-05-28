700410f13c2d11bf42a4b12b8507950b6909871c
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2025-05-28 23:48:22 +0200

    Extract jump button ID to JumpButton

--- src/JumpButton.cpp
+++ src/JumpButton.cpp
@@ -1,11 +1,13 @@
 #include "JumpButton.h"
 using namespace geode::prelude;
 
+std::string const& JumpButton::id = "jump-button"_spr;
+
 CCMenuItemSpriteExtra* JumpButton::create(cocos2d::CCObject* target, cocos2d::SEL_MenuHandler callback, float scale)
 {
     auto sprite = CCMenuItemSpriteExtra::create(CCSprite::create("jump-button.png"_spr), target, callback);
     sprite->m_baseScale = scale; //sets onmouseup target scale
     sprite->setScale(sprite->m_baseScale); //sets initial scale
-    sprite->setID("jump-button"_spr);
+    sprite->setID(JumpButton::id);
     return sprite;
 }
--- src/JumpButton.h
+++ src/JumpButton.h
@@ -4,4 +4,6 @@
 namespace JumpButton
 {
     CCMenuItemSpriteExtra* create(cocos2d::CCObject* target, cocos2d::SEL_MenuHandler callback, float scale = 1.0f);
+
+    extern std::string const& id;
 }
